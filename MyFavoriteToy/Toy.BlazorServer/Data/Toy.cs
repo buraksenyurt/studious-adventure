e95415783211d186eb2bffad87b5b07cb917d33b
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-08 23:28:12 +0300

    Step #11: Toy class added.

---
+++ MyFavoriteToy/Toy.BlazorServer/Data/Toy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Toy.BlazorServer.Data
+{
+    public class Toy
+    {
+        public int ToyId { get; set; }
+        [Required]
+        [MinLength(10,ErrorMessage ="Yaratıcı düşün. Güzel bir oyuncak adı ver")]
+        [MaxLength(30,ErrorMessage ="O kadar da uzun bir isim olmasın")]
+        public string Nickname { get; set; }
+        [Required]
+        [MinLength(20, ErrorMessage = "Yaratıcı düşün. Onun hakkında daha fazla şey söyle")]
+        [MaxLength(250, ErrorMessage = "O kadar da uzun bir açıklama olmasın")]
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public int Like { get; set; }
+        public string Photo { get; set; }
+    }
+}
