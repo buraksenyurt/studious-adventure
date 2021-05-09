2ad7955f274b15f537e0016a583953f861e0e2b7
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-09 16:46:04 +0300

    Step #15: EditToy razor component added.

---
+++ MyFavoriteToy/Toy.BlazorServer/Pages/EditToy.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using Toy.BlazorServer.Data;
+
+namespace Toy.BlazorServer.Pages
+{
+    public partial class EditToy
+    {
+        [Parameter]
+        public ToyModel Toy { get; set; }
+        private ToyModel ToyBackup { get; set; }
+        public bool IsPreviewMode { get; set; } = false;
+
+        protected override void OnInitialized()
+        {
+            ToyBackup = new ToyModel
+            {
+                ToyId=Toy.ToyId,
+                Nickname=Toy.Nickname,
+                Description=Toy.Description,
+                Photo=Toy.Photo,
+                Like=Toy.Like,
+                LastUpdated=Toy.LastUpdated
+            };
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            await _toyService.UpdateAsync(Toy);
+            IsPreviewMode = true;
+            _appState.SetAppState(Toy);
+
+        }
+
+        protected void HandleUndoChanges()
+        {
+            IsPreviewMode = true;
+            if(Toy.Nickname.Trim()!=ToyBackup.Nickname.Trim()
+                ||Toy.Description.Trim()!=ToyBackup.Description.Trim())
+            {
+                Toy = ToyBackup;
+                _appState.SetAppState(ToyBackup);
+            }
+        }
+    }
+}
